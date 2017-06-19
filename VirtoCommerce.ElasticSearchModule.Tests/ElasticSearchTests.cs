c16fea84ec60797e31f96035c39d325b0877f0f4
artem-dudarev <ad@virtoway.com>
2017-06-19 11:46:37 +0200

    Updated packages

--- VirtoCommerce.ElasticSearchModule.Tests/ElasticSearchTests.cs
+++ VirtoCommerce.ElasticSearchModule.Tests/ElasticSearchTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using Moq;
 using VirtoCommerce.CoreModule.Search.Tests;
 using VirtoCommerce.Domain.Search;
 using VirtoCommerce.ElasticSearchModule.Data;
-using VirtoCommerce.Platform.Core.Settings;
 using Xunit;
 
 namespace VirtoCommerce.ElasticSearchModule.Tests
@@ -16,13 +14,6 @@ namespace VirtoCommerce.ElasticSearchModule.Tests
             var host = Environment.GetEnvironmentVariable("TestElasticsearchHost") ?? "localhost:9200";
             var provider = new ElasticSearchProvider(new SearchConnection($"server=http://{host};scope=test"), GetSettingsManager());
             return provider;
-        }
-
-        protected virtual ISettingsManager GetSettingsManager()
-        {
-            var mock = new Mock<ISettingsManager>();
-            mock.Setup(s => s.GetValue(It.IsAny<string>(), It.IsAny<int>())).Returns((string name, int defaultValue) => defaultValue);
-            return mock.Object;
         }
     }
 }
