e3bba6945ddd29016fc7bd010b01681166cf48a1
BelyakovSergey <Belyakov.Sergey@virtoway.com>
2021-09-15 09:58:55 +0300

    PT-1950: Exclude constants from code coverage

--- src/VirtoCommerce.ElasticSearchModule.Web/ModuleConstants.cs
+++ src/VirtoCommerce.ElasticSearchModule.Web/ModuleConstants.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.ElasticSearchModule.Web
 {
+    [ExcludeFromCodeCoverage]
     public static class ModuleConstants
     {
         public static class Settings
