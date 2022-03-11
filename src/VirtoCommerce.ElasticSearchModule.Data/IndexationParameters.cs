c4a94145711db9b0212ab7c53dd703411ac1249e
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-03-11 12:05:08 +0200

    Implement swap and partial update interfaces

---
+++ src/VirtoCommerce.ElasticSearchModule.Data/IndexationParameters.cs
@@ -0,0 +1,8 @@
+namespace VirtoCommerce.ElasticSearchModule.Data
+{
+    public partial class IndexationParameters
+    {
+        public bool Reindex { get; set; }
+        public bool PartialUpdate { get; set; }
+    }
+}
