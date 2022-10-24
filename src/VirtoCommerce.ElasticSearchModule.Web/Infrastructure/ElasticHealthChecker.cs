7c142fc6efab3c41869c8f2ed8554a0411109b32
Andrey Kolchanov <ak@ak.ak>
2022-10-24 11:19:47 +0500

    feat: Introduce ElasticHealthChecker, add timeout tuning options

---
+++ src/VirtoCommerce.ElasticSearchModule.Web/Infrastructure/ElasticHealthChecker.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Nest;
+using VirtoCommerce.ElasticSearchModule.Data;
+
+namespace VirtoCommerce.ElasticSearchModule.Web.Infrastructure
+{
+    public class ElasticHealthChecker : IHealthCheck
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly ElasticSearchOptions _elasticSearchOptions;
+
+        public ElasticHealthChecker(IElasticClient elasticClient, IOptions<ElasticSearchOptions> elasticSearchOptions)
+        {
+            _elasticClient = elasticClient;
+            _elasticSearchOptions = elasticSearchOptions.Value;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            // Use different timeout for the healthchecking pings. Otherwise ping will hangs on default timeout so much longer (default is minute and more).
+            var pingResult = await _elasticClient.PingAsync(
+                new PingRequest() { RequestConfiguration = new RequestConfiguration() { RequestTimeout = TimeSpan.FromSeconds(_elasticSearchOptions.HealthCheckTimeout) } });
+            if (pingResult.IsValid)
+            {
+                return HealthCheckResult.Healthy("Elastic server is reachable");
+            }
+            else
+            {
+                return HealthCheckResult.Unhealthy(@$"No connection to Elastic-server.{Environment.NewLine}{pingResult.ApiCall.DebugInformation}");
+            }
+        }
+    }
+}
