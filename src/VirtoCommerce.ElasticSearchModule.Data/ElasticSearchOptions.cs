c6d74c38b3772f1b58f4cd925f2248e0898784d0
Andrei Kolchanov <akak1977@users.noreply.github.com>
2022-10-25 13:59:31 +0500

    feat: Introduce ElasticHealthChecker, add timeout tuning options (#45)

--- src/VirtoCommerce.ElasticSearchModule.Data/ElasticSearchOptions.cs
+++ src/VirtoCommerce.ElasticSearchModule.Data/ElasticSearchOptions.cs
@@ -8,5 +8,15 @@ namespace VirtoCommerce.ElasticSearchModule.Data
         public bool? EnableHttpCompression { get; set; } = false;
         public bool? EnableCompatibilityMode { get; set; } = false;
         public string CertificateFingerprint { get; set; }
+
+        /// <summary>
+        /// Sets the default timeout in seconds for each request to Elasticsearch. Defaults to 60 seconds.
+        /// </summary>
+        public int RequestTimeout { get; set; } = 60;
+
+        /// <summary>
+        /// Sets the default timeout in seconds for health checking pings to Elasticsearch. Defaults to 2 seconds
+        /// </summary>
+        public int HealthCheckTimeout { get; set; } = 2;
     }
 }
