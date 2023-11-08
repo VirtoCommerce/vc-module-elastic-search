0895b476e1fc508aedb6b9120a4f57634f074c5d
artem-dudarev <ad@virtoway.com>
2023-11-08 12:58:10 +0200

    Support different search provider for each document type

--- src/VirtoCommerce.ElasticSearchModule.Web/Module.cs
+++ src/VirtoCommerce.ElasticSearchModule.Web/Module.cs
@@ -1,17 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nest;
 using VirtoCommerce.ElasticSearchModule.Data;
 using VirtoCommerce.ElasticSearchModule.Web.Infrastructure;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.SearchModule.Core.Extensions;
 using VirtoCommerce.SearchModule.Core.Model;
-using VirtoCommerce.SearchModule.Core.Services;
 
 namespace VirtoCommerce.ElasticSearchModule.Web
 {
@@ -20,26 +18,14 @@ namespace VirtoCommerce.ElasticSearchModule.Web
         public ManifestModuleInfo ModuleInfo { get; set; }
         public IConfiguration Configuration { get; set; }
 
-        private bool IsElasticEnabled
-        {
-            get
-            {
-                var provider = Configuration.GetValue<string>("Search:Provider");
-                return provider.EqualsInvariant("ElasticSearch");
-            }
-        }
-
         public void Initialize(IServiceCollection serviceCollection)
         {
-            if (IsElasticEnabled)
-            {
-                serviceCollection.Configure<ElasticSearchOptions>(Configuration.GetSection("Search:ElasticSearch"));
-                serviceCollection.AddTransient<ElasticSearchRequestBuilder>();
-                serviceCollection.AddSingleton<IConnectionSettingsValues, ElasticSearchConnectionSettings>();
-                serviceCollection.AddSingleton<IElasticClient, ElasticSearchClient>();
-                serviceCollection.AddSingleton<ISearchProvider, ElasticSearchProvider>();
-                serviceCollection.AddHealthChecks().AddCheck<ElasticHealthChecker>("Elastic server connection", tags: new string[] { "Modules", "Elastic" });
-            }
+            serviceCollection.Configure<ElasticSearchOptions>(Configuration.GetSection("Search:ElasticSearch"));
+            serviceCollection.AddTransient<ElasticSearchRequestBuilder>();
+            serviceCollection.AddSingleton<IConnectionSettingsValues, ElasticSearchConnectionSettings>();
+            serviceCollection.AddSingleton<IElasticClient, ElasticSearchClient>();
+            serviceCollection.AddSingleton<ElasticSearchProvider>();
+            serviceCollection.AddHealthChecks().AddCheck<ElasticHealthChecker>("Elastic server connection", tags: new[] { "Modules", "Elastic" });
         }
 
         public void PostInitialize(IApplicationBuilder appBuilder)
@@ -47,14 +33,10 @@ namespace VirtoCommerce.ElasticSearchModule.Web
             var settingsRegistrar = appBuilder.ApplicationServices.GetRequiredService<ISettingsRegistrar>();
             settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
 
-            if (IsElasticEnabled)
-            {
-                var documentConfigs = appBuilder.ApplicationServices.GetRequiredService<IEnumerable<IndexDocumentConfiguration>>();
-                var documentTypes = documentConfigs.Select(c => c.DocumentType).Distinct().ToList();
-
-                var provider = appBuilder.ApplicationServices.GetRequiredService<ISearchProvider>();
-                ((ElasticSearchProvider)provider).AddActiveAlias(documentTypes);
-            }
+            var provider = appBuilder.UseSearchProvider<ElasticSearchProvider>("ElasticSearch");
+            var documentConfigs = appBuilder.ApplicationServices.GetRequiredService<IEnumerable<IndexDocumentConfiguration>>();
+            var documentTypes = documentConfigs.Select(c => c.DocumentType).Distinct();
+            provider.AddActiveAlias(documentTypes);
         }
 
         public void Uninstall()
